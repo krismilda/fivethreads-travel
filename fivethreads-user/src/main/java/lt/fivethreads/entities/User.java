dec5f29435b8527548dfffea807ec6e19d76677c
krismilda <31857297+krismilda@users.noreply.github.com>
2019-03-21 13:11:28 +0200

    Add login and registration (#1)

--- fivethreads-user/src/main/java/lt/fivethreads/entities/User.java
+++ fivethreads-user/src/main/java/lt/fivethreads/entities/User.java
@@ -1,8 +1,15 @@
 package lt.fivethreads.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,8 +23,27 @@ public class User {
     private String password;
 
     @Column(name = "firstName")
-    private String firstName;
+    private String firstname;
 
     @Column(name = "lastName")
     private String lastName;
+
+    @Column (name = "phone")
+    private String phone;
+
+    @ManyToMany
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    public User(){
+    }
+    public User(String firstname, String lastName, String email, String password, String phone) {
+        this.firstname = firstname;
+        this.lastName=lastName;
+        this.email = email;
+        this.password = password;
+        this.phone=phone;
+    }
 }
--- fivethreads-web/src/main/java/lt/fivethreads/web/TravelApplication.java
+++ fivethreads-web/src/main/java/lt/fivethreads/web/TravelApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan({
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EntityScan({
         "lt.fivethreads"
 })
+@EnableJpaRepositories("lt.fivethreads")
 public class TravelApplication {
     public static void main(String[] args) {
         SpringApplication.run(TravelApplication.class, args);
