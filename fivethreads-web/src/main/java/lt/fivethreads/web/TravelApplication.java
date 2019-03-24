193f563cc665e7725ff9cc2cef9cf305b2566654
Matas <silinskas.matas@gmail.com>
2019-03-24 23:27:46 +0200

    Add file controller

--- fivethreads-web/src/main/java/lt/fivethreads/web/TravelApplication.java
+++ fivethreads-web/src/main/java/lt/fivethreads/web/TravelApplication.java
@@ -1,10 +1,14 @@
 package lt.fivethreads.web;
 
+import lt.fivethreads.web.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import javax.annotation.Resource;
 
 @SpringBootApplication
 @ComponentScan({
@@ -14,8 +18,18 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
         "lt.fivethreads"
 })
 @EnableJpaRepositories("lt.fivethreads")
-public class TravelApplication {
+public class TravelApplication implements CommandLineRunner {
+
+    @Resource
+    StorageService storageService;
+
     public static void main(String[] args) {
         SpringApplication.run(TravelApplication.class, args);
+    }
+
+    @Override
+    public void run(String... arg) throws Exception {
+        storageService.deleteAll();
+        storageService.init();
     }
 }
