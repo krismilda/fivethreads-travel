84b1b0b8bfbe136b4cd34fd0f9a9db8cafa1afd2
matas.silinskas <matas.silinskas@visma.com>
2019-05-10 22:16:39 +0300

    Add cross origin to file controllers

--- fivethreads-files/src/main/java/lt/fivethreads/controller/ImportController.java
+++ fivethreads-files/src/main/java/lt/fivethreads/controller/ImportController.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class ImportController {
 
     @Autowired
