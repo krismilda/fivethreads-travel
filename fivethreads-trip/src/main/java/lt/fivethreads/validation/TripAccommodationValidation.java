fbe29a5b8d2b851d4f7654171774de8f289d01cb
Vytautas Krivickas <vytautas1996@gmail.com>
2019-05-31 00:16:31 +0300

    trip accommodation validation fix

--- fivethreads-trip/src/main/java/lt/fivethreads/validation/TripAccommodationValidation.java
+++ fivethreads-trip/src/main/java/lt/fivethreads/validation/TripAccommodationValidation.java
@@ -2,37 +2,59 @@ package lt.fivethreads.validation;
 
 import lt.fivethreads.entities.*;
 import lt.fivethreads.entities.request.FullAddressDTO;
+import lt.fivethreads.exception.CannotCombineTrips;
 import lt.fivethreads.exception.WrongTripData;
 import lt.fivethreads.repositories.NotificationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Component
 public class TripAccommodationValidation {
 
     public void checkFinishStartDates(Date startDate, Date finishDate, String message) {
-        if (finishDate.compareTo(startDate) < 0) {
-            throw new WrongTripData(message);
+        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        try{
+            if (formatter.parse(formatter.format(finishDate)).compareTo(formatter.parse(formatter.format(startDate))) < 0) {
+                throw new WrongTripData(message);
+            }
+        }
+        catch (ParseException e){
+            throw new CannotCombineTrips("Wrong trip dates.");
         }
     }
     public void checkStartDateToday(Date startDate) {
+        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
         Date today = new Date();
-        if (startDate.compareTo(today) < 0) {
-            throw new WrongTripData("Start date is earlier than today.");
+        try{
+            if (formatter.parse(formatter.format(startDate)).compareTo(formatter.parse(formatter.format(today))) < 0) {
+                throw new WrongTripData("Start date is earlier than today.");
+            }
+        }
+        catch (ParseException e){
+            throw new CannotCombineTrips("Wrong trip dates.");
         }
     }
 
     public void checkTripAccommodationDatesAgainstTripDates(Date startDate, Date finishDate, Trip trip){
-        if(startDate.compareTo(trip.getStartDate()) < 0)
-            throw new WrongTripData("Start Date is earlier than trip start date.");
-        if (startDate.compareTo(trip.getFinishDate())  > 0)
-            throw new WrongTripData("Start Date precedes trip start date.");
-        if (finishDate.compareTo(trip.getFinishDate()) > 0)
-            throw new WrongTripData("Finish Date precedes trip finish date.");
-        if (finishDate.compareTo(trip.getStartDate()) < 0)
-            throw new WrongTripData("Finish Date is earlier trip start date.");
+        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        try{
+            if(formatter.parse(formatter.format(startDate)).compareTo(formatter.parse(formatter.format(trip.getStartDate()))) < 0)
+                throw new WrongTripData("Start Date is earlier than trip start date.");
+            if (formatter.parse(formatter.format(startDate)).compareTo(formatter.parse(formatter.format(trip.getFinishDate())))  > 0)
+                throw new WrongTripData("Start Date precedes trip start date.");
+            if (formatter.parse(formatter.format(finishDate)).compareTo(formatter.parse(formatter.format(trip.getFinishDate()))) > 0)
+                throw new WrongTripData("Finish Date precedes trip finish date.");
+            if (formatter.parse(formatter.format(finishDate)).compareTo(formatter.parse(formatter.format(trip.getStartDate()))) < 0)
+                throw new WrongTripData("Finish Date is earlier trip start date.");
+        }
+        catch (ParseException e){
+            throw new CannotCombineTrips("Wrong trip dates.");
+        }
     }
 
     public void hotelRequiredFields(String hotelName, FullAddressDTO hotelAddress, Double price){
